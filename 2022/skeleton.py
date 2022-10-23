9eb729307409a450a3b65136c6ccbfb8ccb0806f
gavilanbe <nahuel@gavilanbe.io>
2022-10-23 21:20:04 +0200

    added skeleton§

--- 2022/skeleton.py
+++ 2022/skeleton.py
@@ -1,10 +1,13 @@
 # --- Skeleton for aoc python code ---
 
+with open('input_X.txt') as f:
+    lines = f.readlines()
+
 def part_one():
-    print("part one is X")
+    print("part one is Y")
 
 def part_two():
-    print("part two is Y")
+    print("part two is Z")
 
 def main():
     part_one()
